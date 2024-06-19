4845cb2ae11364dc92a456f2cc60973365471f8d
Maksym <mysik.bazyk@gmail.com>
2024-06-19 12:45:31 +0300

    add two field in Test.class

---
+++ src/main/java/com/baziuk/SpringSecuredDemo/test/Test.java
@@ -0,0 +1,13 @@
+package com.baziuk.SpringSecuredDemo.test;
+
+public class Test {
+    public static void main(String[] args) {
+        String name = "Anton";
+        String city = "Vinnica";
+
+        System.out.println(city + "-" + name);
+
+    }
+
+
+}
